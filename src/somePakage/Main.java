e3ad9cd776f45deefc14ec8795e5ddbc026e6132
abalmasDA <abalmasit@gmail.com>
2023-07-13 19:44:55 +0300

    Commit1

--- src/somePakage/Main.java
+++ src/somePakage/Main.java
@@ -1,3 +1,10 @@
+package somePakage;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 /**
  * Створіть у пакеті текстовий файл і клас, внесіть у файл якийсь текст.
  * Виведіть на екран вміст файлу
@@ -5,7 +12,7 @@
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String poem = "Як умру, то поховайте\n" +
                 "Мене на могилі\n" +
                 "Серед степу широкого\n" +
@@ -31,10 +38,34 @@ public class Main {
                 "Не забудьте пом’янути\n" +
                 "Незлим тихим словом.";
 
-        
+        FileWriter fileWriter = null;
+        FileReader fileReader = null;
 
+        try {
+            fileWriter = new FileWriter("C:\\Users\\abalm\\odrive\\Google Drive IT\\IteaJavaPro\\TxtFile\\src\\somePakage\\SomeTxtFile.txt");
+            for (int i = 0; i < poem.length(); i++) {
+                fileWriter.write(poem.charAt(i));
+            }
+            System.out.println("Файл записаний!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            fileWriter.close();
+        }
 
-        System.out.println(poem);
+        try {
+            fileReader = new FileReader("C:\\Users\\abalm\\odrive\\Google Drive IT\\IteaJavaPro\\TxtFile\\src\\somePakage\\SomeTxtFile.txt");
+            int symbol = 0;
+            while ((symbol = fileReader.read()) != -1) {
+                System.out.print((char) symbol);
+            }
+            System.out.println();
+            System.out.println("Файл прочитаний!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            fileReader.close();
+        }
     }
 
 }
